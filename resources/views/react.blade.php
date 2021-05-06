1df666e69a33e7b213825dc2cdbe870e69454d0a
Muhammad Farag <muhammad.farag@shopify.com>
2021-05-06 00:34:41 -0400

    If app is installed show React view

---
+++ resources/views/react.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<head>
+</head>
+<body>
+<div id="app"></div>
+<script src="{{ asset('js/app.js') }}"></script>
+
+</body>
+</html>
--- resources/views/unauthenticated.blade.php
+++
@@ -1,1 +0,0 @@
-<h1>This is an unauthenticated view</h1>
