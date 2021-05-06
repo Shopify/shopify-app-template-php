c301ce2c8067ca2c008e3623c06ec80cb20e4d08
Muhammad Farag <muhammad.farag@shopify.com>
2021-05-06 02:14:07 -0400

    Get and log session token

--- resources/js/react/App.jsx
+++ resources/js/react/App.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import createApp from "@shopify/app-bridge";
+import { getSessionToken } from "@shopify/app-bridge-utils"
 
-function App() {
+function App({shop, apiKey}) {
+    const app = createApp({
+        apiKey: apiKey,
+        shopOrigin: shop
+    });
+    getSessionToken(app).then((token) => {
+        console.log(token);
+    });
+
     return (
         <h1>Hello React</h1>
     );
@@ -9,6 +19,7 @@ function App() {
 
 export default App;
 
-if (document.getElementById('app')) {
-    ReactDOM.render(<App />, document.getElementById('app'));
+let appElement = document.getElementById('app');
+if (appElement) {
+    ReactDOM.render(<App {...(appElement.dataset)}/>, appElement);
 }
