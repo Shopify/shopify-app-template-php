42e905f9470ca1c1bdffd614744a364a657e6067
Muhammad Farag <muhammad.farag@shopify.com>
2021-05-06 00:26:40 -0400

    Enable React

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,8 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
+mix.js('resources/js/app.jsx', 'public/js')
+    .react()
     .postCss('resources/css/app.css', 'public/css', [
         //
     ]);
