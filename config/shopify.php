4cbee9acfb282ed85555618b5e34740c515653f3
Sam Schmidt <sam.schmidt@shopify.com>
2021-10-13 11:22:15 -0400

    remove config

--- config/shopify.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-return [
-    'api_key' => env('SHOPIFY_API_KEY', 'YOUR_API_KEY_HERE'),
-    'api_secret' => env('SHOPIFY_API_SECRET', 'YOUR_API_SECRET_HERE'),
-    'scopes' => env('SHOPIFY_SCOPES', [' ']),
-];
