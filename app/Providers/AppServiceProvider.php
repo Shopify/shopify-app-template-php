aaaaa9883956a779d599616f644139589a00080f
Sam Schmidt <sam.schmidt@shopify.com>
2021-10-14 09:12:17 -0400

    force https on local env

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,13 +2,9 @@
 
 namespace App\Providers;
 
-use App\Lib\DbSessionStorage;
-use App\Lib\Handlers\AppUninstalled;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\URL;
-use Shopify\Context;
-use Shopify\Webhooks\Registry;
-use Shopify\Webhooks\Topics;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -32,7 +28,9 @@ class AppServiceProvider extends ServiceProvider
     {
         // We force this because even though Ngrok will come over HTTPS, Laravel sees it as HTTP and will mix content.
         // If you have certificates configured with your local dev url, this is not necessary.
-        URL::forceScheme('https');
+        if (App::environment('local')) {
+            URL::forceScheme('https');
+        }
         // Registry::addHandler(Topics::APP_UNINSTALLED, new AppUninstalled());
     }
 }
