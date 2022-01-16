d67fd10842edddd55e29a1ba31234e9f2415f203
Oussama GHAIEB <Oussama.BelGhaieb@gmail.com>
2022-01-16 20:21:14 +0100

    Update shopify.php

--- config/shopify.php
+++ config/shopify.php
@@ -1,7 +1,7 @@
 <?php 
 
 return [
-    'api_key' => env('SHOPIFY_API_KEY'),
-    'api_secret' => env('SHOPIFY_API_SECRET'),
-    'scopes' => env('SCOPES'),
+    'api_key' => env('SHOPIFY_API_KEY', ''),
+    'api_secret' => env('SHOPIFY_API_SECRET', ''),
+    'scopes' => env('SCOPES', ['']),
 ];
