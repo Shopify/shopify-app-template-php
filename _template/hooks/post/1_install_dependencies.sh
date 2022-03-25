f3d82e0de81e105cdceaea929d7942e9c654575c
Richard Powell <richard.powell@shopify.com>
2022-03-25 14:41:41 -0400

    Add internal shopify files

---
+++ _template/hooks/post/1_install_dependencies.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd $SCRIPT_DIR
+composer install
+
+exit 0
---
+++ _template/hooks/pre/2_check_composer_installed.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+env composer --version > /dev/null
+
+exit $?
