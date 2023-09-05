9aac8ccc3fd6a0de8371cc46987f4436c48dec49
Graham Sutton <grahamsutton2@gmail.com>
2023-09-05 13:55:47 -0400

    #466 - Enable unit and feature testing.

---
+++ web/tests/TestCase.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests;
+
+use App\Http\Middleware\EnsureShopifyInstalled;
+use App\Http\Middleware\EnsureShopifySession;
+use Firebase\JWT\JWT;
+use Illuminate\Contracts\Auth\Authenticatable;
+use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Config;
+
+abstract class TestCase extends BaseTestCase
+{
+    use CreatesApplication;
+
+    /**
+     * Override the actingAs method to inject Shopify specific testing capabilities.
+     *
+     * Disables Shopify middleware and generates a mock Bearer token to successfully
+     * authenticate against Shopify for testing purposes.
+     *
+     * @param \Illuminate\Contracts\Auth\Authenticatable $user
+     * @param string|null $guard
+     *
+     * @return self
+     */
+    public function actingAs(Authenticatable $session, $guard = null): self
+    {
+        $this->withoutMiddleware([
+            EnsureShopifyInstalled::class,
+            EnsureShopifySession::class
+        ]);
+
+        $shopifySecret = Config::get('services.shopify.secret');
+
+        $token = JWT::encode(['dest' => $session->shop], $shopifySecret, 'HS256');
+
+        return parent::actingAs($session, $guard)->withToken($token);
+    }
+}
